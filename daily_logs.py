2b301d080cd0b9c0348d5544f03690dcd2280a94
Jérôme Maréchal <jerome-marechal@proton.me>
2026-01-08 07:16:03 +0100

    update assignment

--- daily_logs.py
+++ daily_logs.py
@@ -1,33 +1,31 @@
 # Two daily logs of (userId, pageId) pairs.
 # Return the list of users who visited on both days and accessed ≥2 distinct pages across the two days.
  
-#day_one = [[1,1],[2,1]]
-#day_two = [[1,1],[2,2]]
-day_one = [[1, 1], [1, 2],[2, 1]]
-day_two = [[1, 3],[2, 2]]
+day_one = [[1,1],[2,1]]
+day_two = [[1,1],[2,2]]
+#day_one = [[1, 1], [1, 2],[2, 1]]
+#day_two = [[1, 3],[2, 2]]
 
 def is_loyal_customer(day_one, day_two):
     report = {}
-    day_count = 0
+    result = []
 
     for userId, pageId in day_one:
         if userId not in report:
-            report[userId] = [set(), set()]
-        report[userId][0].add(pageId)
-        report[userId][1].add(1)
-    day_count += 1    
+            report[userId] = set()
+        report[userId].add(pageId)
 
-    for userId, pageId in day_two:
-        if userId not in report:
-            report[userId] = [set(), 0]
-        report[userId][0].add(pageId)
-        report[userId][1].add(2)
-    day_count += 1    
+    if len(day_two) < 2 and report[day_two[0][1]] not in report[day_two[0][0]]:
+        return [day_two[0][0]]
+    else :
+        for userId, pageId in day_two:
+            if userId not in report:
+                report[userId] = set()
+            report[userId].add(pageId)
+            if len(report[userId]) >= 2:
+                result.append(userId)
+        return result        
 
-    #print(report)
-    #print(day_count)
-    result = [userId for userId, data in report.items() if len(data[0]) > 1 and max(data[1]) == day_count]
-    print(result)
-
-is_loyal_customer(day_one, day_two)    
+loyal_user = is_loyal_customer(day_one, day_two)
+print(loyal_user)
 
