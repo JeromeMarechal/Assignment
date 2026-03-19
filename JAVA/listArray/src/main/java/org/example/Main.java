1064e57fc70fcf0124c1a801eae435701e3926ad
Jérôme Maréchal <jerome-marechal@proton.me>
2026-03-19 16:43:01 +0100

    assignment completed

---
+++ JAVA/listArray/src/main/java/org/example/Main.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 6;
+        ArrayList<Integer> fibo = new ArrayList<>();
+
+        fibo.add(0);
+        fibo.add(1);
+
+        for (int i = 2; i < n; i++) {
+            fibo.add(fibo.get(i - 2) + fibo.get(i - 1));
+        };
+
+        System.out.println(fibo);
+
+    }
+}
--- canFinish.py
+++ canFinish.py
@@ -1,22 +1,30 @@
 def canFinish(numCourses, prerequisites) -> bool:
-    if len(prerequisites) < 2:
-        return True
-    
-    path = set()
-    
-    for pair in prerequisites:
-        if pair[1] in path:
-            if pair[0] in path:
-                return False
-        path.add(pair[0])
-    return True
+    list_of_list = [[] for _ in range(numCourses)]
+    for course, pre in prerequisites:
+        list_of_list[course].append(pre)
+
+    state = [0] * numCourses
+
+    def depth_search(course):
+        if state[course] == 1:
+            return False
         
-            
-            
-    
-    
-          
-    
+        if state[course] == 2:
+            return True
+        
+        if state[course] == 0:
+            state[course] = 1
+            for pre in list_of_list[course]:
+                if depth_search(pre) == False:
+                    return False
+            state[course] = 2
+            return True
+        
+    for course in range(numCourses):
+        if depth_search(course) == False:
+            return False
+
+    return True
     
 # Examples:
 print(canFinish(2, [[1,0]]))  # True
@@ -27,3 +35,4 @@ print(canFinish(4, [[0,1],[1,2],[2,3],[3,1]]))  # False
 print(canFinish(5, [[0,1],[1,2],[2,3],[3,4]]))  # True
 print(canFinish(2, [[0,1],[1,0]]))  # False    
 print(canFinish(12, [[1,0],[2,1],[3,2],[4,3],[5,4],[6,5],[7,6],[8,7],[9,8],[10,9],[11,10]]))  # True
+
